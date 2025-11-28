b655ead16a848427e89f759d9cb5bc8f38276740
Jonatt-001 <1jonatttech@gmail.com>
2025-11-28 17:37:11 +0100

    Revise description in Features component
    Updated the description text in Features component.

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -6,20 +6,17 @@ export const Features = () => {
     <div className="bg-black text-white py-[72px] sm:py-24 ">
 
       <div className="container">
-        <h2 className="text-center font-bold text-5xl sm:text-6xl tracking-tighter">Everything you need </h2>
+        <h2 className="text-center font-bold text-5xl sm:text-6xl tracking-tighter">Everything you need</h2>
         <div className='max-w-xl mx-auto'>
         <p className="text-center mt-5 text-xl text-white/70">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, odit in facilis mollitia dolorum rerum.
+          A unified ecosystem designed for builders. KaaliDev delivers fast workflows, powerful automation, and seamless tools engineered for modern creation.
         </p>
         </div>
         <div className="flex flex-col items-center justify-center sm:flex-row gap-4 mt-32">
           <Bentodemo />
-          
-
         </div>
 
       </div>
-
 
     </div>
   )
